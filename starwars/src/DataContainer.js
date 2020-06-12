8b19ef79f3aa37fd482b5040cc654a84db625ab5
Eorium <travisgent312@gmail.com>
2020-06-12 12:12:11 -0600

    add finished sprint

--- starwars/src/DataContainer.js
+++ starwars/src/DataContainer.js
@@ -6,24 +6,35 @@ import styled from "styled-components";
 const DivContainer = styled.div`
     width: 64%;
     height: 36%;
-    background-color: #10141a;
+    background-color: rgba(20, 22, 28, .95);
     padding-top: 2%;
     margin: 1% auto;
     border-radius: 15px;
 `;
 const DivInfo = styled.div`
     width: 96%;
-    height: 54%;
-    background-color: #10141a;
+    height: 100%;
     margin: 0 auto;
-    font-size: 1.25rem;
+    font-size: 2rem;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    
+`;
+const ImgCharacter = styled.img`
+    width: 64%;
+    height: 80%;
+    margin: 0 auto;
 `;
 
 const DataContainer = props => {
 // set up state for the likes
     return (
         <DivContainer>
-            <DivInfo>Test Letters</DivInfo>
+            <DivInfo>
+                {props.myData.name}
+                <ImgCharacter src={props.myData.image}></ImgCharacter>
+            </DivInfo>
         </DivContainer>
     );
 };
